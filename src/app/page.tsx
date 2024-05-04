3f8e6a3650b27019f61d5b08fa14083bab3ffcef
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-05-05 00:31:48 +0200

    prettier format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,15 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center text-white">
       <div className="container flex max-w-[70rem] flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          <a
-            href="https://minionah.com"
-            target="_blank"
-            className="underline underline-offset-1"
-          >
+          <a href="https://minionah.com" target="_blank" className="underline underline-offset-1">
             MinionAH{" "}
           </a>
           Next
         </h1>
         <p className="text-center text-2xl">
-          This is a NextJS project for MinionAH. It is used to build stuff
-          MinionAH needs that can't be done with Svelte/SvelteKit because a
-          package isn't available or the ecosystem isn't mature enough.
-          <br /><br />
+          This is a NextJS project for MinionAH. It is used to build stuff MinionAH needs that can't be done with Svelte/SvelteKit because a package isn't available or the ecosystem isn't mature enough.
+          <br />
+          <br />
           This project currently includes:
         </p>
 
@@ -26,11 +21,7 @@ export default function HomePage() {
         </ul>
         <p className="text-center text-2xl">
           Open-source on{" "}
-          <a
-            href="https://github.com/DarthGigi/MinionAH-OG"
-            target="_blank"
-            className="underline underline-offset-1"
-          >
+          <a href="https://github.com/DarthGigi/MinionAH-OG" target="_blank" className="underline underline-offset-1">
             GitHub
           </a>
           .
