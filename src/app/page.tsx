d48a90eb6a29bb032e44306161a407b928b996f4
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-05-04 19:05:11 +0200

    chore: Update landing page banner and project description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,36 @@
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+      <div className="container flex max-w-[70rem] flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          <a href="https://minionah.com" target="_blank" className="underline underline-offset-1">
+          <a
+            href="https://minionah.com"
+            target="_blank"
+            className="underline underline-offset-1"
+          >
             MinionAH{" "}
           </a>
           Next
         </h1>
         <p className="text-center text-2xl">
-          This is a NextJS project for MinionAH. It is used to build stuff MinionAH needs that can't be done with Svelte/SvelteKit because a package isn't available or the ecosystem isn't mature enough.
-        </p>
-        <p className="text-left text-2xl">
+          This is a NextJS project for MinionAH. It is used to build stuff
+          MinionAH needs that can't be done with Svelte/SvelteKit because a
+          package isn't available or the ecosystem isn't mature enough.
+          <br /><br />
           This project currently includes:
-          <ul className="list-disc list-inside">
-            <li>OG - This is an Open Graph image generator</li>
-            <li>Resend - This is an email sending service</li>
-          </ul>
         </p>
+
+        <ul className="list-inside list-disc text-left text-2xl">
+          <li>OG - This is an Open Graph image generator</li>
+          <li>Resend - This is an email sending service</li>
+        </ul>
         <p className="text-center text-2xl">
           Open-source on{" "}
-          <a href="https://github.com/DarthGigi/MinionAH-OG" target="_blank" className="underline underline-offset-1">
+          <a
+            href="https://github.com/DarthGigi/MinionAH-OG"
+            target="_blank"
+            className="underline underline-offset-1"
+          >
             GitHub
           </a>
           .
