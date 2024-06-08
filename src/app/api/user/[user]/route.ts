d06bfc60205e02a84c047648e0b255b0f0e7c541
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-06-08 15:32:12 +0200

    fix: linting errors

--- src/app/api/user/[user]/route.ts
+++ src/app/api/user/[user]/route.ts
@@ -2,7 +2,7 @@ import { prisma } from "~/server/prisma";
 
 export const runtime = "edge";
 
-export async function GET({ params }: { params: { user: string } }) {
+export async function GET(_request: Request, { params }: { params: { user: string } }) {
   try {
     const data = await prisma.user.findUnique({
       where: { username: params.user },
